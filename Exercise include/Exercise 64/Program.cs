afc33f966d11b70917b5366726d04a41a16108a3
Ksiuni Lime <possible.live@bk.ru>
2022-12-01 23:27:10 +0300

    Exercise 64

---
+++ Exercise include/Exercise 64/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные 
+// числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+
+string GetPrint(int begin, int finish)
+{
+    if (begin == 1)
+    {
+        return begin.ToString();
+        //  "1";
+    }
+
+
+    Console.WriteLine($"f({begin},{finish}) -> {(begin - 1) + " , " + GetPrint(begin - 1, finish)}");
+    return (begin - 1) + " , " + GetPrint(begin - 1, finish);
+}
+
+
+
+Console.Write("Вводим n: ");
+int n = int.Parse(Console.ReadLine()!);
+int m = 1;
+Console.WriteLine(GetPrint(n, m));
+
+
+
